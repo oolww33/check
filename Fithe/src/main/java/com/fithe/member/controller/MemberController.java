7f8c61abaf91e88593c5fda040e27634c57a3580
kosmo_23 <kosmo_23@DESKTOP-FQIVV37>
2021-02-15 16:28:37 +0900

    123

--- Fithe/src/main/java/com/fithe/member/controller/MemberController.java
+++ Fithe/src/main/java/com/fithe/member/controller/MemberController.java
@@ -161,26 +161,26 @@ public class MemberController {
 		return result;
 	}
 	
-	@RequestMapping(value="/mail")
-	public String mailSending(HttpServletRequest request) {
-		String setform = "oolww11@gmail.com";
-		String tomail = request.getParameter("tomail");
-		String title = request.getParameter("title");
-		String content = request.getParameter("content");
-		
-		try {
-			  MimeMessage message = mailSender.createMimeMessage();
-			  MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
-			  messageHelper.setFrom(setform);  // 보내는사람 생략하거나 하면 정상작동을 안함
-			  messageHelper.setTo(tomail);     // 받는사람 이메일
-			  messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
-			  messageHelper.setText(content);  // 메일 내용
-			
-			  mailSender.send(message);
-
-			} catch(Exception e){
-			  System.out.println(e);
-			}
-		return "email";
-	}
+//	@RequestMapping(value="/mail")
+//	public String mailSending(HttpServletRequest request) {
+//		String setform = "oolww11@gmail.com";
+//		String tomail = request.getParameter("tomail");
+//		String title = request.getParameter("title");
+//		String content = request.getParameter("content");
+//		
+//		try {
+//			  MimeMessage message = mailSender.createMimeMessage();
+//			  MimeMessageHelper messageHelper = new MimeMessageHelper(message, true, "UTF-8");
+//			  messageHelper.setFrom(setform);  // 보내는사람 생략하거나 하면 정상작동을 안함
+//			  messageHelper.setTo(tomail);     // 받는사람 이메일
+//			  messageHelper.setSubject(title); // 메일제목은 생략이 가능하다
+//			  messageHelper.setText(content);  // 메일 내용
+//			
+//			  mailSender.send(message);
+//
+//			} catch(Exception e){
+//			  System.out.println(e);
+//			}
+//		return "email";
+//	}
 }
